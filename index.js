30863631491678ae27caaf0d522aea4b36cfe862
Kadams35 <kacyadams35@gmail.com>
2021-11-16 18:16:40 -0500

    Semantic Scholar api work - unfinished

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const customError = (data) => {
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  base: ['base', 'from', 'coin'],
-  quote: ['quote', 'to', 'market'],
+  SemanticScholarId: ['SemanticScholarId'],
   endpoint: false
 }
 
@@ -21,34 +20,21 @@ const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
+  const author = validator.validated.data.SemanticScholarId
+  const url = `https://api.semanticscholar.org/graph/v1/author/${author}`
+  const fields = "hIndex,paperCount"
 
   const params = {
-    fsym,
-    tsyms
+    fields
   }
 
-  // This is where you would add method and headers
-  // you can add method like GET or POST and add it to the config
-  // The default is GET requests
-  // method = 'get' 
-  // headers = 'headers.....'
   const config = {
     url,
     params
   }
 
-  // The Requester allows API calls be retry in case of timeout
-  // or connection failure
   Requester.request(config, customError)
     .then(response => {
-      // It's common practice to store the desired value at the top-level
-      // result key. This allows different adapters to be compatible with
-      // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
       callback(response.status, Requester.success(jobRunID, response))
     })
     .catch(error => {
